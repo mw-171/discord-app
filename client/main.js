6c4c7a5803687e66dda6592721c23b2d37b230df
mw-171 <meganbxwu@gmail.com>
2024-04-10 23:56:42 -0400

    lil progress

--- client/main.js
+++ client/main.js
@@ -1,8 +1,8 @@
 // Import the SDK
 import { DiscordSDK } from "@discord/embedded-app-sdk";
 
-import './style.css'
-import rocketLogo from '/rocket.png'
+import "./style.css";
+import rocketLogo from "/rocket.png";
 
 // Will eventually store the authenticated user's access_token
 let auth;
@@ -29,10 +29,7 @@ async function setupDiscordSdk() {
     response_type: "code",
     state: "",
     prompt: "none",
-    scope: [
-      "identify",
-      "guilds",
-    ],
+    scope: ["identify", "guilds"],
   });
 
   // Retrieve an access_token from your activity's server
@@ -57,7 +54,7 @@ async function setupDiscordSdk() {
   }
 }
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <img src="${rocketLogo}" class="logo" alt="Discord" />
     <h1>Hello, World!!!</h1>
@@ -65,15 +62,17 @@ document.querySelector('#app').innerHTML = `
 `;
 
 async function appendVoiceChannelName() {
-  const app = document.querySelector('#app');
+  const app = document.querySelector("#app");
 
-  let activityChannelName = 'Unknown';
+  let activityChannelName = "Unknown";
 
   // Requesting the channel in GDMs (when the guild ID is null) requires
   // the dm_channels.read scope which requires Discord approval.
   if (discordSdk.channelId != null && discordSdk.guildId != null) {
     // Over RPC collect info about the channel
-    const channel = await discordSdk.commands.getChannel({channel_id: discordSdk.channelId});
+    const channel = await discordSdk.commands.getChannel({
+      channel_id: discordSdk.channelId,
+    });
     if (channel.name != null) {
       activityChannelName = channel.name;
     }
@@ -81,20 +80,21 @@ async function appendVoiceChannelName() {
 
   // Update the UI with the name of the current voice channel
   const textTagString = `Activity Channel: "${activityChannelName}"`;
-  const textTag = document.createElement('p');
+  const textTag = document.createElement("p");
   textTag.textContent = textTagString;
   app.appendChild(textTag);
 }
 
-async function appendGuildAvatar() { //a guild is a server in api terms
-  const app = document.querySelector('#app');
+async function appendGuildAvatar() {
+  //a guild is a server in api terms
+  const app = document.querySelector("#app");
 
   // 1. From the HTTP API fetch a list of all of the user's guilds
   const guilds = await fetch(`https://discord.com/api/v10/users/@me/guilds`, {
     headers: {
       // NOTE: we're using the access_token provided by the "authenticate" command
       Authorization: `Bearer ${auth.access_token}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   }).then((response) => response.json());
 
@@ -103,40 +103,46 @@ async function appendGuildAvatar() { //a guild is a server in api terms
 
   // 3. Append to the UI an img tag with the related information
   if (currentGuild != null) {
-    const guildImg = document.createElement('img');
+    const guildImg = document.createElement("img");
     guildImg.setAttribute(
-      'src',
+      "src",
       // More info on image formatting here: https://discord.com/developers/docs/reference#image-formatting
       `https://cdn.discordapp.com/icons/${currentGuild.id}/${currentGuild.icon}.webp?size=128`
     );
-    guildImg.setAttribute('width', '128px');
-    guildImg.setAttribute('height', '128px');
-    guildImg.setAttribute('style', 'border-radius: 50%;');
+    guildImg.setAttribute("width", "128px");
+    guildImg.setAttribute("height", "128px");
+    guildImg.setAttribute("style", "border-radius: 50%;");
     app.appendChild(guildImg);
   }
 }
 
 async function appendInCallParticipants() {
-        const app = document.querySelector('#app');
+  const app = document.querySelector("#app");
 
-        const testElement = document.createElement('div');
-        testElement.textContent = 'Function was called';
-        app.appendChild(testElement);
+  const testElement = document.createElement("div");
+  testElement.textContent = "Function was called";
+  app.appendChild(testElement);
 
-        const participants = await discordSdk.commands.getInstanceConnectedParticipants();
-        for (const participant of participants) {
-            const user = await discordSdk.users.get(participant.userId);
-            const userElement1 = document.createElement('div');
-            userElement1.textContent = 'users' + user.username;
-            app.appendChild(userElement1);
-        }
-        const voiceStates = await discordSdk.guilds.get(discordSdk.guildId).getVoiceStates();
-    
-        for (const voiceState of voiceStates) {
-            const user = await discordSdk.users.get(voiceState.userId);
-    
-            const userElement = document.createElement('div');
-            userElement.textContent = 'users' + user.username;
-            app.appendChild(userElement);
-        }
+  const participants =
+    await discordSdk.commands.getInstanceConnectedParticipants();
+  const connectedParticipants = document.createElement("div");
+  connectedParticipants.textContent =
+    "hmm: " + participants.username + " " + participants.id; //why are thgese undefined??
+  app.appendChild(connectedParticipants);
+  // for (const participant of participants) {
+  //     const userElement1 = document.createElement('div');
+  //     userElement1.textContent = 'users' + participant.username;
+  //     app.appendChild(userElement1);
+  // }
+  const voiceStates = await discordSdk.guilds
+    .get(discordSdk.guildId)
+    .getVoiceStates();
+
+  for (const voiceState of voiceStates) {
+    const user = await discordSdk.users.get(voiceState.userId);
+
+    const userElement = document.createElement("div");
+    userElement.textContent = "users" + user.username;
+    app.appendChild(userElement);
+  }
 }
